b4a2a38f64483896144186b1aaf75850519b187c
jayjaycastin <judy.luo@broadridge.com>
2021-10-18 23:48:03 +0000

    w5d1

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,10 @@
 
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
@@ -8,6 +14,7 @@ get '/signup' do     # if a user navigates to the path "/signup",
     @user = User.new   # setup empty @user object
     erb(:signup)       # render "app/views/signup.erb"
 end
+
 
 post '/signup' do
      # grab user input values from params
@@ -21,10 +28,34 @@ post '/signup' do
 
   # if user validations pass and user is saved
   if @user.save
-    "User #{username} saved!"
+    redirect to('/login')
   else
     erb(:signup)
   end
+end
+
+get '/login' do    # when a GET request comes into /login
+  erb(:login)      # render app/views/login.erb
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
 end
 
 =begin
